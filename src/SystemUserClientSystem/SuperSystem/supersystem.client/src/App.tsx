632826fac888d786262e5e739f208983ae0a249e
Rune T. Larsen <rune@hjemmekino.no>
2024-09-17 13:10:32 +0200

    More design changes

--- src/SystemUserClientSystem/SuperSystem/supersystem.client/src/App.tsx
+++ src/SystemUserClientSystem/SuperSystem/supersystem.client/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import './tailwind.css';
 import imagelogo from './assets/illustration.jpg';
-import smartlogo from './assets/SmartCloudLogo.png';
+import smartlogo from './assets/SmartCloudLogo.svg';
 import { Card } from '@digdir/designsystemet-react';
 
 interface Forecast {
@@ -48,10 +48,10 @@ function App() {
                 {/* Hero Section */}
                 <header className="bg-smartcloud text-white">
                     <div className="container mx-auto px-4 py-12 overflow-auto">
-                        <img src={smartlogo} alt="Smart Cloud Logo" className="w-auto mx-auto mb-4 float-left h-24"  />
-                        <div className="float-right">
-                            <button className="text-blue-600 px-4 py-2 rounded-lg shadow-md hover:bg-blue-500 hover:text-white transition mr-2">Login</button>
-                            <button className="bg-white text-blue-600 px-4 py-2 rounded-3xl shadow-md hover:bg-blue-500 hover:text-white transition">Register</button>
+                        <img src={smartlogo} alt="Smart Cloud Logo" className="w-auto mx-auto mb-2 float-left h-28"  />
+                        <div className="float-right pt-16">
+                            <button className="text-white px-4 py-2 rounded-lg hover:bg-blue-500 hover:text-white transition mr-2">Logg inn</button>
+                            <button className="bg-white text-blue-600 px-4 py-2 rounded-3xl shadow-md hover:bg-blue-500 hover:text-white transition">Prøv gratis</button>
                         </div>
                     </div>
                 </header>
@@ -63,8 +63,8 @@ function App() {
                                 <img src={imagelogo} alt="Illustration" />
                             </div>
                             <div>
-                                <h2 className="text-6xl">Jobb smartere med SmartCloud</h2>
-                                <p className="text-xl">Med SmartCloud får du jobben gjort på dine egne premisser. Vårt system er fullt integrert med Altinn, noe som gjør at du kan fokusere på andre ting, mens dine egne tilpassede moduler gjør jobben.</p>
+                                <h2 className="text-6xl font-semibold font-color-cloudblue py-4">Jobb smartere med SmartCloud</h2>
+                                <p className="text-xl py-4">Med SmartCloud får du jobben gjort på dine egne premisser. Vårt system er fullt integrert med Altinn, noe som gjør at du kan fokusere på andre ting, mens dine egne tilpassede moduler gjør jobben.</p>
                             </div>
                         </div>
                     </div>
