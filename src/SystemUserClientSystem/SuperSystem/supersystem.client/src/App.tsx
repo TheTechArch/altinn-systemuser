daffa208f1b8d60ea55c495b4b36a58e6a2b117f
Rune T. Larsen <rune@hjemmekino.no>
2024-09-17 14:13:41 +0200

    More updates

--- src/SystemUserClientSystem/SuperSystem/supersystem.client/src/App.tsx
+++ src/SystemUserClientSystem/SuperSystem/supersystem.client/src/App.tsx
@@ -1,46 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 import './tailwind.css';
 import imagelogo from './assets/illustration.jpg';
 import smartlogo from './assets/SmartCloudLogo.svg';
-import { Card } from '@digdir/designsystemet-react';
-
-interface Forecast {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
-}
 
 function App() {
-    const [forecasts, setForecasts] = useState<Forecast[]>();
+
 
     useEffect(() => {
-        populateWeatherData();
     }, []);
-
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tableLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
 
     return (
         <div>
@@ -119,6 +87,7 @@ function App() {
                     </div>
                 </section>
 
+
                 {/* Footer */}
                 <footer className="bg-gray-800 text-white py-6">
                     <div className="container mx-auto px-4 text-center">
@@ -126,21 +95,8 @@ function App() {
                     </div>
                 </footer>
             </div>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
-            <Card>
-                <Card.Header>Tittel</Card.Header>
-                <Card.Content>Innhold</Card.Content>
-                <Card.Footer>Valgfri fotnote</Card.Footer>
-            </Card>
         </div>
     );
-
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        setForecasts(data);
-    }
 }
 
 export default App;
